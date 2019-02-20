b5383a5c541469c6febe714c817ed01c75a65e7a
Aries <aries@asterism.xyz>
2019-02-21 00:34:51 +0900

    Fix readme

--- install.sh
+++ install.sh
@@ -2,22 +2,30 @@
 set -eu
 
 # Create partition
-sgdisk --clear /dev/sda
+sgdisk -o /dev/sda
 sgdisk --new "0::+512M" /dev/sda
-sgdisk --new /dev/sda
+sgdisk --new "0::0" /dev/sda
 
 # Format and mount filesystem
-
+mkfs.vfat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 
 # Select a mirror
 cp /etc/pacman.d/mirrorlist /tmp/mirrorlist
 grep "\.jp" /tmp/mirrorlist > /etc/pacman.d/mirrorlist
 
 # Install base system
-
+pacstrap /mnt base base-devel
 
 # Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 
+# Create setup script
+cat << _EOF_ >> /mnt/setup.sh
+#!/bin/bash
 
 # locale
 
@@ -33,5 +41,12 @@ grep "\.jp" /tmp/mirrorlist > /etc/pacman.d/mirrorlist
 
 # Bootloader
 
+_EOF_
 
 
+# Run setup script
+arch-chroot /mnt /mnt/setup.sh
+
+# Finish
+umount -R /mnt
+reboot
